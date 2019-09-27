670cd35a76611df79df580357768ec85c539f326
vinod Pande <vinodkpande2014@gmail.com>
2019-09-27 16:27:53 +0530

    Initial commit

---
+++ app/src/main/java/com/tutorial/service_ex/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tutorial.service_ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // Start the service
+    public void startService(View view) {
+        startService(new Intent(this, AppService.class));
+    }
+    // Stop the service
+    public void stopService(View view) {
+        stopService(new Intent(this, AppService.class));
+    }
+}
